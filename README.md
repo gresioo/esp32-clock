06c22788bba1e9043ed2a7fba54d932eee2a6602
Grzegorz Wyszyński <gresioo@gmail.com>
2020-01-04 23:17:56 +0100

    Added first prototypes of functions

--- README.md
+++ README.md
@@ -5,3 +5,4 @@ Stuff used:
 https://github.com/espressif/esp-idf/tree/master/examples/wifi/smart_config
 https://ipstack.com/documentation
 https://developer.airly.eu/docs#endpoints.measurements
+https://cdn.boxtec.ch/pub/sure/DE-DP106_Ver1.0_EN.pdf
---
+++ include/esp-clock.h
@@ -0,0 +1,29 @@
+/**
+ * @file esp-clock.h
+ * @author Grzegorz Wyszyński (gresioo@gmail.com)
+ * @brief Header for esp32-clock basing on Sure 3208 display
+ * @version 0.1
+ * @date 2020-01-04
+ * 
+ * @copyright Copyright (c) 2020
+ * 
+ */
+
+
+/**
+ * @brief Sends binary data to display device
+ * 
+ * @param data all the binary data to be sent to device
+ * @param lengthOfData length of data
+ * @return Returns error code
+ */ 
+int sendToDisplay(int * data,int lengthOfData)
+/**
+ * @brief Writes one char to display device
+ * 
+ * 
+ * @param textin Char to be displayed
+ * @param position Where to position the char (in lines)
+ * @return Returns error code
+ */
+int writeToDisplay(char textin,int position)

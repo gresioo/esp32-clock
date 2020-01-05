b60cc162423c6c03aaa02677125a08269e09e9f0
Grzegorz Wyszyński <gresioo@gmail.com>
2020-01-05 22:46:20 +0100

    First not working version of interface with HT1632

--- src/esp-clock.c
+++ src/esp-clock.c
@@ -1,20 +1,32 @@
-#define DISPLAY_CS 14
-#define DISPLAY_WR 15
-#define DISPLAY_DATA 16
+#define DISPLAY_CS 23
+#define DISPLAY_WR 5
+#define DISPLAY_DATA 18
+#define GPIO_OUTPUT_PIN_SEL  ((1ULL<<DISPLAY_CS) | (1ULL<<DISPLAY_WR) | (1ULL<<DISPLAY_DATA))
+#include "freertos/FreeRTOS.h"
+#include "freertos/task.h"
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include "freertos/queue.h"
+#include "driver/gpio.h"
 
 esp_err_t waitForAWhile()
 {
-    vTaskDelay(10/portTICK_PERIOD_MS);
+    // vTaskDelay(1/portTICK_PERIOD_MS);
     return ESP_OK;
 }
 
 esp_err_t sendOneBit(uint32_t level)
 {
-    gpio_set_level(DISPLAY_WR,1);
-    gpio_set_level(DISPLAY_DATA,level);
-    waitForAWhile();
     gpio_set_level(DISPLAY_WR,0);
     waitForAWhile();
+    gpio_set_level(DISPLAY_DATA,level);
+    waitForAWhile();
+    gpio_set_level(DISPLAY_WR,1);
+    waitForAWhile();
+    waitForAWhile();
+    printf("%i",level);
+    return ESP_OK;
 }
 
 esp_err_t sendToDisplay(int8_t command,int * data,int lengthOfData)
@@ -23,6 +35,8 @@ esp_err_t sendToDisplay(int8_t command,int * data,int lengthOfData)
     int column; //column to be stored and then sent out
     // CS to high
     gpio_set_level(DISPLAY_CS,1);
+    waitForAWhile();
+    gpio_set_level(DISPLAY_CS,0);
     //sending command (3 bits)
     for (i=0;i<3;i++){
         sendOneBit((command&(1<<i))/(1<<i)); //send ith bit
@@ -35,11 +49,40 @@ esp_err_t sendToDisplay(int8_t command,int * data,int lengthOfData)
         column=data[i];
         for (j=0;j<8;j++)
         {       
-            sendOneBit((column&(1<<i))/(1<<i)); //send ith bit
+            sendOneBit((column&(1<<j))/(1<<j)); //send jth bit
         }
     }
-    return ESP_OK
+    gpio_set_level(DISPLAY_CS,1);
+    return ESP_OK;
 }
 
+void app_main()
+{
+    gpio_config_t io_conf;
+    //disable interrupt
+    io_conf.intr_type = GPIO_PIN_INTR_DISABLE;
+    //set as output mode
+    io_conf.mode = GPIO_MODE_OUTPUT;
+    //bit mask of the pins that you want to set,e.g.GPIO18/19
+    io_conf.pin_bit_mask = GPIO_OUTPUT_PIN_SEL;
+    //disable pull-down mode
+    io_conf.pull_down_en = 0;
+    //disable pull-up mode
+    io_conf.pull_up_en = 0;
+    //configure GPIO with the given settings
+    gpio_config(&io_conf);
 
+    int i,j;
+    int LEDstate[32];
+    for(i=0;i<8;i++)
+    {
+        for(j=0;j<32;j++)
+        {
+            LEDstate[j]=1<<i;
+        }
+        printf("\naaa\n");
+        sendToDisplay(0b101,LEDstate,32);
+        vTaskDelay(1000/portTICK_PERIOD_MS);
+    }
+}
 
